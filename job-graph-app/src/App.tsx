793da85c158bda20ae6d14d15d9b96029e8b6bce
Hemanth Srivathsav <hemanthsrivathsav@gmail.com>
2025-11-12 17:25:06 +0530

    first push

---
+++ job-graph-app/src/App.tsx
@@ -0,0 +1,11 @@
+import JobGraphCanvas from "./components/JobGraphCanvas";
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <JobGraphCanvas />
+    </div>
+  );
+}
+
+export default App;
