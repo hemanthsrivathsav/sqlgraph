623b0ca20af5a8132e62fe02c277ff2888f09c3c
Hemanth Srivathsav <hemanthsrivathsav@gmail.com>
2025-11-12 18:40:24 +0530

    fix working

---
+++ sql-graph/src/App.tsx
@@ -0,0 +1,9 @@
+import JobGraphCanvas from "./components/JobGraphCanvas";
+
+export default function App() {
+  return (
+    <div className="w-full h-screen">
+      <JobGraphCanvas />
+    </div>
+  );
+}
